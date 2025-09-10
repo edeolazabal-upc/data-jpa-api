b59580402e6d80a1cdbe7b061e42fb9070535014
edeolazabal-urp <edgar.deolazabal@urp.edu.pe>
2025-09-10 18:33:45 -0500

    primer commit

---
+++ src/main/java/com/upc/data_jpa_api/DataJpaApiApplication.java
@@ -0,0 +1,13 @@
+package com.upc.data_jpa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/data_jpa_api/DataJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.data_jpa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
